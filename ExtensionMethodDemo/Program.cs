35fdefb2a3accb7fe7d0e758d1ec0e29e64064a8
developer <developer@DESKTOP-PFHID3B>
2020-06-01 18:25:25 +0500

    Add project files.

---
+++ ExtensionMethodDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person()
+            {
+                Name = "Sophia",
+                Gender = "Female"
+            };
+
+            p1.GoToSleep();
+            p1.WakeUp();
+            p1.Eat();
+            p1.GoToWork();
+            p1.SayHello();
+            Console.ReadLine();
+
+        }
+    }
+
+    public static class PersonExtension
+    {
+        public static void SayHello(this Person p1)
+        {
+            Console.WriteLine("{0} Says Hello", p1.Name);
+        }
+    }
+}
---
+++ MyLib/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLib
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("{0} is Eating", Name);
+        }
+        public void GoToSleep()
+        {
+            Console.WriteLine("{0} is sleeping", Name);
+        }
+        public void WakeUp()
+        {
+            Console.WriteLine("{0} just waked up", Name);
+        }
+
+        public void GoToWork()
+        {
+            Console.WriteLine("{0} is at work", Name);
+        }
+        
+
+    }
+}
